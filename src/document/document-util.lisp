849ae416216e3ff32b4a016f48de2e8a7fc43ba0
Gábor Melis <mega@retes.hu>
2022-12-02 10:56:14 +0000

    move away from deprecated mathjax CDN url

--- src/document/document-util.lisp
+++ src/document/document-util.lisp
@@ -199,8 +199,8 @@
        }
      });
    </script>
-   <script type="text/javascript" ~
-    src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML">
+   <script type="text/javascript" async
+     src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML">
    </script>
    </head>~%~
    <body>~%~
