a54b283f8c1f298a0b1f620186d4cec46cd35175
Gábor Melis <mega@retes.hu>
2023-08-22 17:31:46 +0200

    make the dependency graph of .asd files acyclic
    ... to help OS-level packaging (see
    https://github.com/melisgl/try/issues/5).

--- package.lisp
+++ package.lisp
@@ -7,11 +7,11 @@
                (sb-ext:muffle-conditions sb-kernel::package-at-variance))
     (handler-bind
         (#+sbcl (sb-kernel::package-at-variance #'muffle-warning))
-      ;; Foreshadow some symbols that MGL-PAX/BOOTSTRAP interns, which
+      ;; Foreshadow some symbols that MGL-PAX-BOOTSTRAP interns, which
       ;; would otherwise lead to conflicts when DREF-EXT is finally
       ;; :USEd. The alternative of SHADOWING-IMPORTing these just
       ;; before (USE-PACKAGE :DREF-EXT) would be annoying because in
-      ;; this case recompiling MGL-PAX/BOOTSTRAP dumps a fasl with
+      ;; this case recompiling MGL-PAX-BOOTSTRAP dumps a fasl with
       ;; references to DREF-EXT:LOCATIVE-TYPE in it, which then fails
       ;; to load into a fresh lisp because DREF-EXT is not yet
       ;; defined.
@@ -29,7 +29,7 @@
         (:documentation "See MGL-PAX::@PAX-MANUAL.")
         (:use #:common-lisp #:dref #:dref-ext)
         (:nicknames #:pax)
-        ;; These are the exports from MGL-PAX/BOOTSTRAP.
+        ;; These are the exports from MGL-PAX-BOOTSTRAP.
         (:export #:define-package
          #:defsection #:section
          #:define-glossary-term #:glossary-term
