aed0186a48d53ac708e7eef1f132a8ba36bb4703
Estevan Castilho <tevo@riajuu.net>
2025-05-10 02:45:43 +0000

    clarify `with-autoloading`...
    ...and fix a bug related to the capturing of the enclosed lexical
    scope of `body`, while we're at it.

--- src/base/autoload.lisp
+++ src/base/autoload.lisp
@@ -4,8 +4,9 @@
   (value nil))
 
 (defmacro with-autoloading
-    ((asdf-system-name &optional (name asdf-system-name)
-                         (first-time-p (gensym "FIRST-TIME-P")))
+    ((asdf-system-name
+      &optional (name asdf-system-name)
+        (first-time-p (gensym "FIRST-TIME-P") first-time-provided-p))
      &body body)
   (let* ((box (gensym "BOX")) (actual-body (gensym "ACTUAL-BODY"))
          (args (gensym "ARGS"))
@@ -18,14 +19,47 @@
                         (lambda (&rest ,args)
                           (declare (ignore ,args))
                           (error "Autoloading ~S failed." ',name)))
+
                   (when (asdf:load-system ',asdf-system-name)
-                    (setf (box-value ,box) ,actual-body))
-                  (funcall (box-value ,box) ,box t))))
+                    ;; If the system loads correctly, set the function
+                    ;; within BOX to a trampoline to the actual body of
+                    ;; the WITH-LOADING form. Note that we cannot simply
+                    ;; store the current value of ACTUAL-BODY within BOX
+                    ;; because that would mean the lexical scope of BODY
+                    ;; would always correspond to the outer scope at the
+                    ;; time of the *first* invocation, and not any
+                    ;; subsequent ones.
+                    (setf (box-value ,box)
+                          (lambda (,box ,first-time-p ,actual-body)
+                            (declare (ignore ,box))
+                            (funcall ,actual-body ,first-time-p))))
+
+                  ;; Finally, call the actual body, as appropriate.
+                  (funcall (box-value ,box) ,box t ,actual-body))))
+    ;; At load time, we create a BOX object containing a function
+    ;; corresponding to the body to be executed by this WITH-AUTOLOADING
+    ;; form. The initial value is a function that attempts to load the
+    ;; ASDF system ASDF-SYSTEM-NAME, setting the value within BOX to
+    ;; either a function signalling that autoloading failed, or a
+    ;; trampoline to the provided BODY, depending on whether the system
+    ;; loaded successfully or not. Further executions will always invoke
+    ;; either the signalling function or the trampoline, without
+    ;; attempting to load the ASDF system again.
+    ;;
+    ;; The value of the load-time-constructed box object can be thought
+    ;; as behaving similarly to a C `static' variable - a binding whose
+    ;; value persists between invocations.
     `(let ((,box (load-time-value (make-box :value ,stub))))
        (funcall (box-value ,box) ,box nil
-                (lambda (,box ,first-time-p &rest ,args)
+                ;; We have to pass the actual body as an argument
+                ;; because it needs to be compiled within the lexical
+                ;; scope of the body it is enclosed under, rather than
+                ;; the null lexical scope enforced within the
+                ;; LOAD-TIME-VALUE form.
+                (lambda (,first-time-p)
                   (declare (ignore ,box ,args)
-                           (ignorable ,first-time-p))
+                           ,@(unless first-time-provided-p
+                               (list `(ignore ,first-time-p))))
                   ,@body)))))
 
 ;;; Define a function with NAME that loads ASDF-SYSTEM-NAME (neither
