30105c257bc0e1df1ea310cff8667520d790cbef
Gábor Melis <mega@retes.hu>
2026-06-17 15:22:37 +0200

    do not clobber condition definitions on reload
    If mgl-pax was reloaded for some reason, then its dummy
    DEFINE-CONDITIONs redefined the full definitions if they were already
    loaded.

--- src/base/transcribe-early.lisp
+++ src/base/transcribe-early.lisp
@@ -1,19 +1,25 @@
 (in-package :mgl-pax)
 
-;;; Silence SBCL compiler notes.
+;;; Silence SBCL compiler notes about undefined types when these are
+;;; used in a condition handler.
 #+sbcl
-(define-condition transcription-error (error condition-context-mixin) ())
-
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-class 'transcription-error nil)
+    (define-condition transcription-error (error condition-context-mixin) ())))
 #+sbcl
-(define-condition transcription-consistency-error (transcription-error)
-  ())
-
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-class 'transcription-consistency-error nil)
+    (define-condition transcription-consistency-error (transcription-error)
+      ())))
 #+sbcl
-(define-condition transcription-values-consistency-error
-    (transcription-consistency-error)
-  ())
-
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-class 'transcription-values-consistency-error nil)
+    (define-condition transcription-values-consistency-error
+        (transcription-consistency-error)
+      ())))
 #+sbcl
-(define-condition transcription-output-consistency-error
-    (transcription-consistency-error)
-  ())
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-class 'transcription-output-consistency-error nil)
+    (define-condition transcription-output-consistency-error
+        (transcription-consistency-error)
+      ())))
